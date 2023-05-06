5a4f7a821c552afe5cce44e82ce214aba9d66489
Daniel <leinadpy@gmail.com>
2023-05-06 13:19:13 -0400

    Fix para deploys 6 node 14.18.0 sin env

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,15 +1,15 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyCbMOkpWC956XLWUTGw-Iki8SOXF3gzGEI',
+  authDomain: 'marie-nutri-app-d3211.firebaseapp.com',
+  projectId: 'marie-nutri-app-d3211',
+  storageBucket: 'marie-nutri-app-d3211.appspot.com',
+  messagingSenderId: '233354107182',
+  appId: '1:233354107182:web:23355846e5ac7c80ca02bf',
 };
 
 firebase.initializeApp(firebaseConfig);
